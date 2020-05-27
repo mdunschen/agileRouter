ca00af728736863a5b97d11f2906380e497ff0c7
mdunschen <mdunschen@gmail.com>
2020-05-27 17:00:55 +0100

    Update agileRouterAjax.py
    Removed addresses

--- agileRouterAjax.py
+++ agileRouterAjax.py
@@ -38,15 +38,6 @@ def main(adrInput, useOneWay, progress=None):
 
 if __name__ == "__main__":
     addresses = """
-    66 Mount Pleasant, L3 5SD;
-    92 Quebec Quay, L3 4ER;
-    22 Percy Street, L8 7LU;
-    16 Aigburth Drive, L17 4JG;
-    10 Rundle Road, L17 0AG;
-    94 Blythswood St, L17 7DG;
-    52 Woodlands Road, L17 0AP;
-    100 Belgrave Road, L17 7AH;
-    29 Wembley Rd, L18 2DP;
-    5 Ramilies Road, L18 1EE;"""
+    """
 
     print(main(addresses, False))
